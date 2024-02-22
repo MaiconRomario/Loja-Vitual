b3cbb0023fe0a21b7016b60bfe5af045edc62ef6
MaiconRomario <maiconromario389@hotmail.com>
2024-02-22 00:16:46 -0300

    Crud Imagens de produtos

---
+++ backend/src/main/java/com/devmaicon/backend/repositories/ProdutoImagensRepository.java
@@ -0,0 +1,9 @@
+package com.devmaicon.backend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devmaicon.backend.entities.ProdutoImagens;
+
+public interface ProdutoImagensRepository extends JpaRepository<ProdutoImagens, Long> {
+
+}
